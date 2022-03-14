f80912fae144f32762a0a77ad6a7559ff8f4f8f9
Seweryn Pajor <serwer3103@gmail.com>
2022-03-14 20:11:25 +0100

    Naprawa obrazka kolejki gracza w kółko i krzyżyk

--- Assets/Scripts/KManager.cs
+++ Assets/Scripts/KManager.cs
@@ -26,7 +26,7 @@ public class KManager : MonoBehaviour
         {
             pola[i].image.sprite = grafiki[0];
         }
-	TerazKolej.overrideSprite = grafiki[1];
+	TerazKolej.sprite = grafiki[1];
     }
 
     void Update()
@@ -51,162 +51,25 @@ public class KManager : MonoBehaviour
     }
 
     
-    public int ChangePole(int ktore, int grafika)
+    public void ChangeField(int which)
     {
 
         if (kolejka == 1)
         {
             kolejka = 2;
-            pola[ktore].image.sprite = grafiki[1];
+            pola[which].image.sprite = grafiki[1];
+	    ipola[which] = 1;
+	    TerazKolej.sprite = grafiki[2];
       
         }
         else if (kolejka == 2)
         {
             kolejka = 1;
-            pola[ktore].image.sprite = grafiki[2];
-        }
-        return 0;
+	    ipola[which] = 2;
+            pola[which].image.sprite = grafiki[2];
+	    TerazKolej.sprite = grafiki[1];
+	}
     }
-    public void zmienpole1(int na_co)
-    {
-        if (kolejka == 1)
-        {
-            kolejka = 2;
-            pola[1].image.sprite = grafiki[1];
-            ipola[1] = 1;
-        }
-        else if (kolejka == 2)
-        {
-            kolejka = 1;
-            pola[1].image.sprite = grafiki[2];
-            ipola[1] = 2;
-        }
-    }
-    public void zmienpole0(int na_co)
-    {
-        if (kolejka == 1)
-        {
-            kolejka = 2;
-            pola[0].image.sprite = grafiki[1];
-            ipola[0] = 1;
-        }
-        else if (kolejka == 2)
-        {
-            kolejka = 1;
-            pola[0].image.sprite = grafiki[2];
-            ipola[0] = 2;
-        }
-    }
-    public void zmienpole2(int na_co)
-    {
-        if (kolejka == 1)
-        {
-            kolejka = 2;
-            ipola[2] = 1;
-            pola[2].image.sprite = grafiki[1];
-        }
-        else if (kolejka == 2)
-        {
-            kolejka = 1;
-            pola[2].image.sprite = grafiki[2];
-            ipola[2] = 2;
-        }
-    }
-    public void zmienpole3(int na_co)
-    {
-        if (kolejka == 1)
-        {
-            ipola[3] = 1;
-            kolejka = 2;
-            pola[3].image.sprite = grafiki[1];
-        }
-        else if (kolejka == 2)
-        {
-            kolejka = 1;
-            ipola[3] = 2;
-            pola[3].image.sprite = grafiki[2];
-        }
-    }
-    public void zmienpole4(int na_co)
-    {
-        if (kolejka == 1)
-        {
-            ipola[4] = 1;
-            kolejka = 2;
-            pola[4].image.sprite = grafiki[1];
-        }
-        else if (kolejka == 2)
-        {
-            kolejka = 1;
-            ipola[4] = 2;
-            pola[4].image.sprite = grafiki[2];
-        }
-    }
-    public void zmienpole5(int na_co)
-    {
-        if (kolejka == 1)
-        {
-            ipola[5] = 1;
-            kolejka = 2;
-            pola[5].image.sprite = grafiki[1];
-        }
-        else if (kolejka == 2)
-        {
-            kolejka = 1;
-            ipola[5] = 2;
-            pola[5].image.sprite = grafiki[2];
-        }
-    }
-    public void zmienpole6(int na_co)
-    {
-
-        if (kolejka == 1)
-        {
-            ipola[6] = 1;
-            kolejka = 2;
-            pola[6].image.sprite = grafiki[1];
-        }
-        else if (kolejka == 2)
-        {
-            kolejka = 1;
-            pola[6].image.sprite = grafiki[2];
-            ipola[6] = 2;
-        }
-    }
-    public void zmienpole7(int grafik)
-    {
-
-
-        if (kolejka == 1)
-        {
-            ipola[7] = 1;
-            kolejka = 2;
-            pola[7].image.sprite = grafiki[1];
-        }
-        else if (kolejka == 2)
-        {
-            kolejka = 1;
-            ipola[7] = 2;
-            pola[7].image.sprite = grafiki[2];
-        }
-    }
-    public void zmienpole8(int grafik)
-    {
-
-        if (kolejka == 1)
-        {
-            ipola[8] = 1;
-            kolejka = 2;
-            pola[8].image.sprite = grafiki[1];
-        }
-        else if (kolejka == 2)
-        {
-            kolejka = 1;
-            ipola[8] = 2;
-            pola[8].image.sprite = grafiki[2];
-        }
-    }
-
     public void Wygrana()
     {
         if (ipola[0] == 1 && ipola[1] == 1 && ipola[2] == 1)
